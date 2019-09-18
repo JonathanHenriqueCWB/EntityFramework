db6ed8748ebd9f3b9693a582e893b68fa79e33d2
Jonathan Alves <jonathanhenriquecwb@gmail.com>
2019-09-18 15:04:11 -0300

    Projeto vendas entity framework

---
+++ BancoDados/DAL/CategoriaDAO.cs
@@ -0,0 +1,14 @@
+﻿using BancoDados.Models;
+using System.Linq;
+namespace BancoDados.DAL
+{
+    class CategoriaDAO
+    {
+        private static Context ctx = SingletonContext.GetInstance();
+
+        public static Categoria BuscarCategoriaPorNome
+            (Categoria c) =>
+            ctx.Categorias.FirstOrDefault
+            (x => x.Nome.Equals(c.Nome));
+    }
+}
